7c850f6d186adcb3c877b9631c660b9cd078e8cd
Passing25 <127103009+Passing25@users.noreply.github.com>
2023-05-15 09:55:23 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>Material Design for Bootstrap</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+    <!-- Google Fonts Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+    <!-- MDB -->
+    <link rel="stylesheet" href="css/mdb.min.css" />
+    <!-- Custom styles -->
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    <!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+
+    <footer style="background-color: #deded5;">
+    <div class="container p-4">
+      <div class="row">
+        <div class="col-lg-6 col-md-12 mb-4">
+          <h5 class="mb-3" style="letter-spacing: 2px; color: #818963;">Le temple du Savoir</h5>
+          <p>
+          Fondée en 1974, l'université de Ouagadougou est située <br>
+           dans le domaine de Zogona à Ouagadougou, 
+           <br>la capitale du Burkina Faso. Le premier recteur de 
+          <br> l'université fût le professeur Yembila Abdoulaye Toguyeni.
+          </p>
+        </div>
+        <div class="col-lg-3 col-md-6 mb-4">
+          <h5 class="mb-1" style="letter-spacing: 2px; color: #818963;">Heure d'ouverture</h5>
+          <table class="table" style="color: #4f4f4f; border-color: #666;">
+            <tbody>
+              <tr>
+                <td>Lun - Ven:</td>
+                <td>8h - 18h</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2023 Copyright:
+      <a class="text-dark" href="https://mdbootstrap.com/">By Digituma Agency</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  
+</div>
+<!-- End of .container -->
+    <!-- MDB -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <!-- Custom scripts -->
+    <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
+
---
+++ script/script.js
@@ -0,0 +1,162 @@
+// creating the date of birth lists
+var months = 12
+var days = 31
+var today = new Date()
+var year = today.getFullYear() +1 
+maxYears = 70
+
+	//Creating the month list
+	for (var i = 1; i <= months; i++) {
+		var monthList = document.getElementById('month')
+		// creating the option element
+		var option = document.createElement('option')
+		// adding the value attributr
+		option.setAttribute('value', i)
+
+		// appending the month
+		option.innerHTML = i
+		//appending the options to thee list
+		monthList.appendChild(option)
+
+	} 
+
+
+	//Creating the dayes list
+	for (var i = 1; i <= days; i++) {
+
+		// getting the id of the drop-list
+		var dayList = document.getElementById('day')
+		// creating the option element
+		var option = document.createElement('option') 
+
+		// adding the value attributr
+		option.setAttribute('value', i)
+		// appending the day
+		option.innerHTML = i
+		//appending the options to thee list
+		dayList.appendChild(option)
+	}
+
+
+	//Creating the Years list
+	for (var i = 0; i < maxYears; i++) {
+		year--
+		
+
+		var yearList = document.getElementById('year')
+		// creating the option element
+		var option = document.createElement('option') 
+
+		// adding the value attributr
+		option.setAttribute('value', year)
+		// appending the year
+		option.innerHTML = year
+		//appending the options to thee list
+		yearList.appendChild(option)
+		
+	}
+
+
+ 
+
+function rigisterProfile() {
+
+
+	//Error output 
+	var errorOutput = document.getElementById('error')
+	//Error messages and RegEx
+	var errorMesGlobal = 'Please, fill out all the filds.'
+	var errorMail = 'Make sure your email address is correct.'
+	var errorPhone = 'Please enter vaild phone number of 10 digits.'
+	var regExMail = /^[a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,5}$/
+	var regExPhone = /^[0-9]{10}/
+
+
+	// an if statment to insure all the filds are entered
+	if (!form.firstName.value || !form.lastName.value || !form.gender.value || !form.email.value || !form.phone.value || !form.bio.value ) {
+		
+		errorOutput.innerHTML = errorMesGlobal
+		return
+	}
+	//check if the email is vaild 
+	if(!regExMail.test(form.email.value)) {
+		errorOutput.innerHTML = errorMail
+		return
+	}
+	//check if the email is vaild
+	if (!regExPhone.test(form.phone.value)) {
+		errorOutput.innerHTML = errorPhone
+		return
+	} 
+	//set the src attribute according to the gender
+	if (form.gender.value == 'male') {
+		imageSrc = 'http://vignette4.wikia.nocookie.net/yogscast/images/f/fd/YOGSCASTRidgedog.png/revision/latest/zoom-crop/width/240/height/240?cb=20131113061509'
+	} else {
+		imageSrc = 'https://upload.wikimedia.org/wikipedia/commons/0/07/Avatar_girl_face.png'
+	}
+	//___________________________________________________________
+
+// creating an object 
+ var userprofile = {
+ 	firstName: form.firstName.value + ' ' + form.lastName.value,
+ 	lastName: form.lastName.value,
+ 	gender: form.gender.value,
+ 	dob: form.month.value + '/' + form.day.value + '/' + form.year.value,
+ 	email: form.email.value,
+ 	phone: form.phone.value,
+ 	bio: form.bio.value
+ }
+
+
+
+ 	var output = document.getElementById('output')
+
+ 	//creating elements
+ 	var image = document.createElement('img')
+ 	var rigisteredName = document.createElement('h3')
+ 	var rigisteredGender = document.createElement('h3')
+ 	var rigisteredDob = document.createElement('h3')
+ 	var rigisteredEmail = document.createElement('h3')
+ 	var rigisteredPhone = document.createElement('h3')
+ 	var rigisteredBio = document.createElement('p')
+
+ 	//adding source for the image
+ 	image.setAttribute('src', imageSrc)
+ 	// creating spans
+ 	var nameSpan = '<span>Username: </span>'
+ 	var genderSpan = '<span>Gender: </span>'
+ 	var dobSpan = '<span>Date of Birth: </span>'
+ 	var emailSpan = '<span>E-mail: </span>'
+ 	var phoneSpan = '<span>Phone: </span>'
+ 	var bioSpan = '<span>Bio: </span>'
+
+ 	//inner.html
+ 	rigisteredName.innerHTML = nameSpan + userprofile.firstName
+ 	rigisteredGender.innerHTML = genderSpan + userprofile.gender
+ 	rigisteredDob.innerHTML = dobSpan + userprofile.dob
+ 	rigisteredEmail.innerHTML = emailSpan + userprofile.email
+ 	rigisteredPhone.innerHTML = phoneSpan + userprofile.phone.replace(/[^\d]+/g, '')
+     .replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3')
+
+ 	rigisteredBio.innerHTML = bioSpan +userprofile.bio
+
+ 	//appending the childs
+ 	output.appendChild(image)
+ 	output.appendChild(rigisteredName)
+ 	output.appendChild(rigisteredGender)
+ 	output.appendChild(rigisteredDob)
+ 	output.appendChild(rigisteredEmail)
+ 	output.appendChild(rigisteredPhone)
+ 	output.appendChild(rigisteredBio)
+
+ 	//hidding the form
+ 	document.getElementById("form").style.display = "none";
+
+ 	//fixing the footer to the bottom
+ 	// document.getElementById('footer').setAttribute('class', 'fixed')
+
+
+}
+
+document.getElementById('rigister').addEventListener('click', rigisterProfile, false)
+

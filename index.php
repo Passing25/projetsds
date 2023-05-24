73be57360652bc1e1f0cf20d3c95961b460b77fe
Passing25 <127103009+Passing25@users.noreply.github.com>
2023-05-24 09:49:06 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"> 
 	<head>
 		<meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,73 +22,35 @@
                                 </h1>
 								<p>Veuillez remplir le formulaire!</p>
 							</div>
-							<form role="form" id="form" name="form">
-							  <div class="form-group name">
+							<form role="form"  method="POST" action="enregister.php">
+							  <div class="form-group">
 							    <label for="firstname">Nom:</label>
-							    <input type="text" name="firstName" class="form-control" id="firstName">
+							    <input type="text" name="nom" class="form-control" id="firstName">
 
 							    <label for="lastName">Prénom:</label>
-							    <input type="text" name="lastName" class="form-control" id="lastName">
+							    <input type="text" name="prenom" class="form-control" id="lastName">
 							  </div>
-							  <!-- Gender -->
-							  <div class="radio gender">
-								  <label><input type="radio" value="male" name="gender">homme</label>
-								
-								  <label><input type="radio" value="female" name="gender">Femme</label>
-								</div>
-
-								<!--date of birth-->
 								<div class="dob">
 									<div class="form-group">
 									    <label for="sel1">Date de Naissance:</label>
-									    <p>JJ/MM/AAAA</p>
-										<select class="form-control" name="day" id="day">
-										   
-										</select>
-
-										<!-- <label for="sel1">Day:</label> -->
-										
-                                        <select class="form-control" name="month" id="month">
-										</select>
-										 <!-- <label for="sel1">year:</label> -->
-										<select class="form-control" name="year" id="year">
-										    
-										</select>
-									</div> <!-- form-group -->
-								</div> <!-- dob -->
-								
-								<!-- phone and mail -->
+									    <input type="date" name="date_de_naissance">
+									</div> 
+								</div>
 								<div class="contacts">
 									<div class="form-group">
 									<!-- PHONE -->
 									  <label for="email">Date d'entrée à l'Université:</label>
-									  <input type="text" class="form-control" name="email" id="email">
+									  <input type="date" class="form-control" name="date_entree" id="email">
 									<!-- PHONE -->
 									  <label for="phone">Personne à prévenir en cas de besoin:</label>
-									  <input type="number" class="form-control" name="phone" id="phone">
+									  <input type="text" class="form-control" name="numero_utile" id="phone">
 									</div> 
-								</div> <!-- contacts -->
-
-							  <!-- bio -->
-							  	<!-- <div class="bio">
-							  		<div class="form-group">
-								 		<label for="bio">Information supplementaire:</label>
-								  		<textarea class="form-control" name="bio" rows="5" id="bio"></textarea>
-									</div>
-								</div> bio -->
-
-								<!--ERROR-->
-
-								<div class="error" id="error"></div>
-
-								<!--Button-->
-								<div>
-									<button type="button" class="btn" id="rigister">Enregistrer</button>
 								</div>
+								<div>
+									<input type="submit" value="Sauvegarder">
+								</div>
+								
 							</form>
-							<!--Ending the FORM-->
-
-							<!--Starting the output-->
 							<div id="imageOutput" class="image-output">
 								
 							</div> <!-- image-output -->
